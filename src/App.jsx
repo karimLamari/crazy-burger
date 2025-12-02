b560e6bdc279510fa1fee76433d8f1f53ec010d1
karimLamari <l.karim9942@gmail.com>
2025-12-02 15:43:56 +0100

    F01 : Login > refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import LoginPage from "./Pages/LoginPage";
+import LoginPage from "./Components/Pages/login/LoginPage";
 
 function App() {
 
