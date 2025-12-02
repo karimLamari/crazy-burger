a9bf8d81bc50e7686574bef3f0a7e32bac670f78
karimLamari <l.karim9942@gmail.com>
2025-12-02 10:12:14 +0100

    F01: Create login page without CSS

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,39 @@
+import {useState} from "react";
+
+function LoginForm() {
+     //state
+        const [prenom, setPrenom] = useState('');
+
+
+    //Comportements: 
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            alert(`bonjour ${prenom} `);
+            setPrenom('');       
+        }
+    //Render
+    
+    return (
+
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="prenom"></label>
+                <input
+                    id="prenom"
+                    type="text"
+                    placeholder="Entrez votre prénom"
+                    value={prenom}
+                    onChange={(e) => setPrenom(e.target.value)}
+                    required />
+                <button
+                    type="submit"
+                >
+                    Accéder a votre espace
+                </button>
+            </form>
+
+        </div>
+    )
+}
+export default LoginForm
---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "../Components/LoginForm";
+function Login() {
+    
+    return (
+
+        <div>
+            <h1> Bienvenue chez nous !</h1>
+            <p> Connectez-vous</p>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default Login
