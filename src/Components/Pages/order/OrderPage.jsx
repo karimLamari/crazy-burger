629fa0ec7fddaa6985f2037b4b771fcc1a4cb776
karimLamari <l.karim9942@gmail.com>
2025-12-08 17:07:09 +0100

    F02:LOGIN > Add redirection to/errorPage on Submit

--- src/Components/Pages/order/OrderPage.jsx
+++ src/Components/Pages/order/OrderPage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 function OrderPage() {
+  const { username } = useParams();
+
   return (
     <>
-      <div>OrderPage</div>
+      <h1>Bonjour {username}</h1>
       <Link to="/">
-        <button >Déconnexion</button>
+        <button>Déconnexion</button>
       </Link>
     </>
-  )
+  );
 }
 
-export default OrderPage
+export default OrderPage;
