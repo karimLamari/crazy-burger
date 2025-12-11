491dc18c18fdc1079768e40d2ff5b5c15ff1ccc7
karimLamari <l.karim9942@gmail.com>
2025-12-11 16:00:59 +0100

    F03: Add login styling and background

--- src/Components/Pages/login/LoginPage.jsx
+++ src/Components/Pages/login/LoginPage.jsx
@@ -1,6 +1,38 @@
 import LoginForm from "./LoginForm";
+import Logo from "../reusable-ui/Logo";
+import styled from "styled-components";
 function Login() {
-
-    return <LoginForm />
+  return (
+    <LoginPageStyled>
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
+  );
 }
-export default Login
+export default Login;
+
+const LoginPageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+
+  &::before {
+    content: "";
+    background:
+        rgba(0, 0, 0, 0.5);
+    background-image:
+        url('/images/background-image.jpg') ;
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+
+`;
