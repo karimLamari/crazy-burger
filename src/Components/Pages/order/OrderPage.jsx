5dd3be2cc8658bd114d0b64d6db3827f1a9da0ca
karimLamari <l.karim9942@gmail.com>
2025-12-12 06:45:13 +0100

    F04: Add navbar and main sections

--- src/Components/Pages/order/OrderPage.jsx
+++ src/Components/Pages/order/OrderPage.jsx
@@ -8,11 +8,17 @@ function OrderPage() {
   return (
     <OrderPageStyled>
       <div className="container">
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
+        <div className="navbar">
+        <h1>Bonjour {username}</h1>
+          navbar
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
 
+        </div>
+        <div className="main">
+          main
+        </div>
       </div>
     </OrderPageStyled>
   );
@@ -24,12 +30,35 @@ const OrderPageStyled=styled.div`
   background-color:orange;
   height:100vh;
   display:flex;
+  flex-direction:column;
   justify-content:center;
   align-items;center;
+  
+  
 
   .container{
-  background:red;
-  height:95vh}
-  max-width:1400px;
-  }
+    background:red;
+    height:95vh;
+    max-width:1400px;
+    display:flex;
+    flex-direction:column;
+
+
+    .navbar{
+      height:10vh;
+      width:100%;
+      background:blue;
+      display:flex;
+      justify-content:space-between;
+      align-items:center;
+    }
+    .main{
+      width:100%;
+      background:green;
+      flex:1;
+    
+    }
+    
+    
+    }
 `
