152dd3eafe1bd0cf0d4b3dc3e1ecaecaec0b4fff
karimLamari <l.karim9942@gmail.com>
2025-12-13 12:38:52 +0100

    F05: Add formatted price

--- src/Components/Pages/order/Main/Main.jsx
+++ src/Components/Pages/order/Main/Main.jsx
@@ -2,31 +2,29 @@ import styled from "styled-components";
 
 import { useState } from "react";
 import Menu from "./Menu";
-import {fakeMenu1,fakeMenu2} from "../../../../data/fakeMenu";
+import { fakeMenu1, fakeMenu2 } from "../../../../data/fakeMenu";
 import { theme } from "../../../../theme";
 
 export default function Main() {
-const [menus, setMenus] = useState([...fakeMenu1, ...fakeMenu2]);
+  const [menus, setMenus] = useState([...fakeMenu1, ...fakeMenu2]);
 
   return (
     <MainStyled>
       {/* <div className="basket"></div> */}
       <Menu menus={menus} />
-
     </MainStyled>
-  )
+  );
 }
 
-const MainStyled=styled.div`
-      display:grid;
-      width:100%;
-      background:${theme.colors.background_white};
-      box-shadow: 0px 8px 20px 8px rgba(0,0,0,0.2) inset;
-        flex:1;
-        border-bottom-left-radius: ${theme.borderRadius.extraRound};
-        border-bottom-right-radius: ${theme.borderRadius.extraRound};
+const MainStyled = styled.div`
+  display: grid;
+  width: 100%;
+  background: ${theme.colors.background_white};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  flex: 1;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
 
-      .basket{
-      }
-
+  .basket {
+  }
 `;
