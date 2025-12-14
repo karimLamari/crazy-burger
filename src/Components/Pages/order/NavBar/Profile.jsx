d7dd93e4ab4234252fe3bf53a0d9f8ceacd80a64
karimLamari <l.karim9942@gmail.com>
2025-12-14 19:37:28 +0100

    ORDER > Correct style NAVBAR

--- src/Components/Pages/order/NavBar/Profile.jsx
+++ src/Components/Pages/order/NavBar/Profile.jsx
@@ -3,51 +3,62 @@ import { Link } from "react-router-dom";
 import { theme } from "../../../../theme/index.js";
 import styled from "styled-components";
 
-function Profile({username} ) {
+function Profile({ username }) {
   return (
-        <ProfileStyled>
-          <div className="profile-info">
-            <p>Hey, <span className="username">{username}</span></p>
-            <Link to="/">
-              <button >Se déconnecter</button>
-            </Link>
-          </div>
-          <BsPersonCircle size={40} color={theme.colors.greyDark} />
-        </ProfileStyled>  )
+    <ProfileStyled>
+      <div className="profile-info">
+        <p>
+          Hey, <span className="username">{username}</span>
+        </p>
+        <Link to="/">
+          <button className="logout-button">Se déconnecter</button>
+        </Link>
+      </div>
+      <BsPersonCircle className="profile-icon" />
+    </ProfileStyled>
+  );
 }
 
 export default Profile;
 
 const ProfileStyled = styled.div`
-display:flex;
-      justify-content:center;
-      align-items:center;
-      padding:  ${theme.spacing.sm};
-      gap:${theme.spacing.sm};
-        .profile-info{
-          display:flex;
-          flex-direction:column;
-          justify-content:center;
-          gap:2px;
-          };
-          p{
-            display:flex;
-            gap:5px;
-            margin:0;
-            font-size:${theme.fonts.P1};
-            color:${theme.colors.greyDark};
-            justify-content:center;
-            align-items:center;
-            }
-          button{
-            border:none;
-            background:transparent;
-            }
-          .username{
-            display:flex;
-            flex-direction:row;
-            font-weight:${theme.weights.bold};
-            color:${theme.colors.primary};
-            }
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left:50px;
+  gap: 10px;
+  .profile-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  p {
+    display: flex;
+    gap: 5px;
+    margin: 0;
+    font-size:16px;
+    font-weight: ${theme.weights.regular};
+    color: ${theme.colors.greyBlue};
+    justify-content: center;
+    align-items: center;
+  }
+  button {
+    border: none;
+    background: transparent;
+  }
+  .username {
+    display: flex;
+    flex-direction: row;
+    font-weight: ${theme.weights.bold};
+    color: ${theme.colors.primary};
+  }
+  .logout-button{
+        color: ${theme.colors.greyBlue};
+        font-size: 10px;
+        cursor: pointer;
+  }
+  .profile-icon{
+    font-size: 36px;
+    color: ${theme.colors.greyBlue};
+  }
 `;
