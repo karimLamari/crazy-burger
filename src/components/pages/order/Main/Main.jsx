0e768a177473d57a1539e4bc6255c35bd3e8ea3b
karimLamari <l.karim9942@gmail.com>
2025-12-15 17:17:40 +0100

    F07: Add layout with basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,29 +1,46 @@
-import styled from "styled-components"
-import { theme } from "../../../../theme"
-import Menu from "./Menu"
+import styled from "styled-components";
+import { theme } from "../../../../theme";
+import Menu from "./Menu";
 
 export default function Main() {
   return (
     <MainStyled className="main">
       {/* <div className="basket">Basket</div> */}
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">Admin</div>
+      </div>
     </MainStyled>
-  )
+  );
 }
 
 const MainStyled = styled.div`
+  border: 2px solid green;
   background: ${theme.colors.background_white};
-  flex: 1; // or you can also use this : height: calc(95vh - 10vh);
-
+  height: calc(95vh - 10vh);
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
   display: grid;
   grid-template-columns: 1fr;
 
-  overflow-y: scroll;
   /* .basket {
     background: pink;
   } */
-`
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+    .admin {
+      background: red;
+      height: 250px;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      left: 0;
+    }
+  }
+`;
