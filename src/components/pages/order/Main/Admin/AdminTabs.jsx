ae2eb04191c5685e3ce1248e87408947700a53de
karimLamari <l.karim9942@gmail.com>
2025-12-16 09:37:42 +0100

    F07 : ADMIN > Add tab component

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
+import Tab from '../../../../reusable-ui/Tab';
+import { FiChevronDown } from 'react-icons/fi';
 
 export default function AdminTabs() {
   return (
-    <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+    <AdminTabsStyled>
+        <Tab Icon={<FiChevronDown />} />
+
+    </AdminTabsStyled>
   )
 }
 
 const AdminTabsStyled = styled.div`
-
+    display:flex;
+    padding:0 20px;
 `;
