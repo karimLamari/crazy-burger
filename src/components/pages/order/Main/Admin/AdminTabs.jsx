8d46c4f25dc2d4f392b55c197cf85d253e3f31af
karimLamari <l.karim9942@gmail.com>
2025-12-16 10:18:05 +0100

    F07: Add tab click and active styles

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,17 +1,30 @@
-import styled from 'styled-components';
-import Tab from '../../../../reusable-ui/Tab';
-import { FiChevronDown } from 'react-icons/fi';
+import styled from "styled-components";
+import Tab from "../../../../reusable-ui/Tab";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { theme } from "../../../../../theme";
 
-export default function AdminTabs() {
+export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
+
   return (
     <AdminTabsStyled>
-        <Tab Icon={<FiChevronDown />} />
+      <Tab
 
+        Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={isCollapsed ? "isActive" : ""}
+      />
     </AdminTabsStyled>
-  )
+  );
 }
 
 const AdminTabsStyled = styled.div`
-    display:flex;
-    padding:0 20px;
+  display: flex;
+  padding: 0 20px;
+
+  .isActive{
+    background:${theme.colors.background_dark};
+    color: ${theme.colors.white};
+    border-color: ${theme.colors.background_dark};
+  }
+  
 `;
--- src/components/reusable-ui/Tab.jsx
+++ src/components/reusable-ui/Tab.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import {theme} from '../../theme'
-export default function Tab({Icon}) {
+export default function Tab({Icon, onClick,className='className'}) {
   return (
-    <TabStyled>
-        <div className="icon">{Icon}</div>
+    <TabStyled onClick={onClick} className={className}>
+        <div className="icon" >{Icon}</div>
     </TabStyled>
   )
 }
